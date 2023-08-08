2900200c68cd509c75b902070e09edfa8be032be
Kamran <136312141+BayramovKamran@users.noreply.github.com>
2023-08-08 15:25:03 +0300

    Update pom.xml

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,6 +1,7 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,10 +9,15 @@ public class AnimalsCage {
 
     @Autowired
     @Qualifier("dog")
-    private Animal animal; 
-     @Autowired
+    private Animal animal;
+
+    @Autowired
     private Timer timer;
-    
+
+    public Timer getTimer() {
+        return timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
